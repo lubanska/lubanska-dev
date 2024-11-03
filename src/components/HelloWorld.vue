0450959c084a990705d79681f2a5c028f41e08c5
Agata <lubanska.a@gmail.com>
2024-11-03 13:27:53 +0100

    feat: generate address on mounted

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const mailUser = ref("");
+const mailDomain = ref("");
+
+onMounted(() => {
+  mailUser.value = "hello";
+  mailDomain.value = "lubanska.dev";
+});
+</script>
 
 <template>
   <div
     class="w-full flex flex-col items-center justify-center mb-8 text-center">
-    <h1 class="mb-8" set-pointer-state="hover">need a dev?</h1>
+    <h1 class="mb-8" set-pointer-state="hover">agata lubanska</h1>
     <p>frontend developer with a flair for design</p>
     <p>
       say hi on
-      <b>hello@lubanska.dev</b>
+      <b>{{ mailUser }}@{{ mailDomain }}</b>
     </p>
   </div>
 </template>
