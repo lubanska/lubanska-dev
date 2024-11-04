8cf46dc779c51161cd355bba11184621b6bc7eef
Agata <lubanska.a@gmail.com>
2024-11-04 10:31:21 +0100

    fix: hide custom pointer on mobile

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import HelloWorld from "./components/HelloWorld.vue";
 import Contact from "./components/Contact.vue";
 import CustomPointer from "./components/CustomPointer.vue";
+
+const isMobile = window.matchMedia("(max-width: 768px)").matches;
 </script>
 
 <template>
-  <CustomPointer v-custom-pointer />
+  <CustomPointer v-if="!isMobile" v-custom-pointer />
   <header>
     <HelloWorld />
   </header>
